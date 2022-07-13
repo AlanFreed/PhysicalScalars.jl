7ca478b5dd31c6c6c6c67616c19d23e19b61cfa3
Alan Freed <alan.d.freed@protonmail.com>
2022-07-13 06:10:18 -0400

    updated to version 0.1.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,4 +40,160 @@ end
     @test toString(e3) == "3.0000E+00 g⋅cm/s²"
 end
 
+@testset "isDimensionless" begin
+    @test PhysicalScalars.isDimensionless(e1) == false
+    @test PhysicalScalars.isDimensionless(newPhysicalScalar(SI_DIMENSIONLESS)) == true
+end
+
+@testset "isCGS" begin
+    @test PhysicalScalars.isCGS(newPhysicalScalar(DYNE)) == true
+    @test PhysicalScalars.isCGS(newPhysicalScalar(PASCAL)) == false
+    @test PhysicalScalars.isCGS(newPhysicalScalar(CGS_DIMENSIONLESS)) == true
+    @test PhysicalScalars.isCGS(newPhysicalScalar(SI_DIMENSIONLESS)) == false
+end
+
+@testset "isSI" begin
+    @test PhysicalScalars.isSI(newPhysicalScalar(DYNE)) == false
+    @test PhysicalScalars.isSI(newPhysicalScalar(PASCAL)) == true
+    @test PhysicalScalars.isSI(newPhysicalScalar(CGS_DIMENSIONLESS)) == false
+    @test PhysicalScalars.isSI(newPhysicalScalar(SI_DIMENSIONLESS)) == true
+end
+
+@testset "toCGS & toSI" begin
+    η = newPhysicalScalar(CGS_ENTROPY_DENSITY)
+    set!(η, 1.22)
+    @test toCGS(toSI(η)) == η
+    σ = newPhysicalScalar(PASCAL)
+    set!(σ, 1.1E+06)
+    @test toSI(toCGS(σ)) == σ
+end
+
+@testset "abs" begin
+    s1 = newPhysicalScalar(ERG)
+    set!(s1, -1.1)
+    s2 = -s1
+    @test PhysicalScalars.abs(s1) == s2
+    @test PhysicalScalars.abs(s2) == s2
+end
+
+@testset "sqrt & √" begin
+    s1 = newPhysicalScalar(SI_AREA)
+    set!(s1, 4.0)
+    s2 = newPhysicalScalar(SI_LENGTH)
+    set!(s2, 2.0)
+    @test sqrt(s1) == √(s1)
+    @test sqrt(s1) == s2
+    @test √(s1) == s2
+    s3 = newPhysicalScalar(CGS_DIMENSIONLESS)
+    set!(s3, 4.0)
+    @test sqrt(s3) == 2.0
+    @test √(s3) == 2.0
+end
+
+@testset "cbrt & ∛" begin
+    s1 = newPhysicalScalar(SI_VOLUME)
+    set!(s1, 8.0)
+    s2 = newPhysicalScalar(SI_LENGTH)
+    set!(s2, 2.0)
+    @test cbrt(s1) == ∛(s1)
+    @test cbrt(s1) == s2
+    @test ∛(s1) == s2
+    s3 = newPhysicalScalar(CGS_DIMENSIONLESS)
+    set!(s3, 8.0)
+    @test cbrt(s3) == 2.0
+    @test ∛(s3) == 2.0
+end
+
+@testset "round" begin
+    s1 = newPhysicalScalar(SI_AREA)
+    s2 = newPhysicalScalar(SI_AREA)
+    set!(s1, 1.45)
+    set!(s2, 1.0)
+    @test round(s1) == s2
+end
+
+@testset "floor" begin
+    s1 = newPhysicalScalar(SI_AREA)
+    s2 = newPhysicalScalar(SI_AREA)
+    set!(s1, 1.45)
+    set!(s2, 1.0)
+    @test floor(s1) == s2
+end
+
+@testset "ceil" begin
+    s1 = newPhysicalScalar(SI_AREA)
+    s2 = newPhysicalScalar(SI_AREA)
+    set!(s1, 1.45)
+    set!(s2, 2.0)
+    @test ceil(s1) == s2
+end
+
+@testset "sin & asin" begin
+    s = newPhysicalScalar(CGS_DIMENSIONLESS)
+    set!(s, π/4)
+    @test sin(s) == sin(π/4)
+    @test asin(s) == asin(π/4)
+end
+
+@testset "cos & acos" begin
+    s = newPhysicalScalar(CGS_DIMENSIONLESS)
+    set!(s, π/4)
+    @test cos(s) == cos(π/4)
+    @test acos(s) == acos(π/4)
+end
+
+@testset "tan & atan" begin
+    s = newPhysicalScalar(CGS_DIMENSIONLESS)
+    set!(s, π/4)
+    @test tan(s) == tan(π/4)
+    @test atan(s) == atan(π/4)
+    x = newPhysicalScalar(METER)
+    y = newPhysicalScalar(METER)
+    set!(x, 4.0)
+    set!(y, π)
+    @test atan(x,y) == atan(4.0,π)
+end
+
+@testset "sinh & asinh" begin
+    s = newPhysicalScalar(CGS_DIMENSIONLESS)
+    set!(s, π)
+    @test sinh(s) == sinh(π)
+    @test asinh(s) == asinh(π)
+end
+
+@testset "cosh & acosh" begin
+    s = newPhysicalScalar(CGS_DIMENSIONLESS)
+    set!(s, π)
+    @test cosh(s) == cosh(π)
+    @test acosh(s) == acosh(π)
+end
+
+@testset "tanh & atanh" begin
+    s = newPhysicalScalar(CGS_DIMENSIONLESS)
+    set!(s, π/4)
+    @test tanh(s) == tanh(π/4)
+    @test atanh(s) == atanh(π/4)
+end
+
+@testset "log & exp" begin
+    s = newPhysicalScalar(CGS_DIMENSIONLESS)
+    set!(s, π)
+    @test log(s) == log(π)
+    @test exp(s) == exp(π)
+end
+
+@testset "log2 & exp2" begin
+    s = newPhysicalScalar(CGS_DIMENSIONLESS)
+    set!(s, π)
+    @test log2(s) == log2(π)
+    @test exp2(s) == exp2(π)
+end
+
+@testset "log10 & exp10" begin
+    s = newPhysicalScalar(CGS_DIMENSIONLESS)
+    set!(s, π)
+    @test log10(s) == log10(π)
+    @test exp10(s) == exp10(π)
+end
+
 end # module runtests
